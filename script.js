4378bce94c3087ae0323a1d5024a3801f9fea1b0
Christine Elaine Lynch <lalainelynch@gmail.com>
2023-07-21 18:34:54 +0800

    added swap button function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const fromText = document.querySelector(".from-text"),
 toText = document.querySelector(".to-text"),
 selectTags = document.querySelectorAll("select"),
 translateBtn = document.querySelector("button");
+exchangeIcon = document.querySelector(".exchange")
 
 selectTags.forEach((selectTag, id) => {
   for (const country_code in countries) {
@@ -26,4 +27,13 @@ translateBtn.addEventListener("click", () => {
     console.log(data);
     toText.value = data.responseData.translatedText;
   });
+});
+
+exchangeIcon.addEventListener("click", () => {
+  let tempText = fromText.value;
+  tempLang = selectTags[0].value;
+  fromText.value = toText.value;
+  selectTags[0].value = selectTags[1].value;
+  toText.value = tempText;
+  selectTags[1].value = tempLang;
 });
